efb97a647b8dc02bd12de253475cf4b5b3ec53a9
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-25 11:05:43 -0700

    adjust

--- src/main/java/com/company/gamestore/controller/InvoiceController.java
+++ src/main/java/com/company/gamestore/controller/InvoiceController.java
@@ -2,10 +2,12 @@ package com.company.gamestore.controller;
 
 import com.company.gamestore.model.Invoice;
 import com.company.gamestore.repository.InvoiceRepository;
+import com.company.gamestore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +16,9 @@ public class InvoiceController {
 
     @Autowired
     InvoiceRepository invoiceRepository;
+
+    @Autowired
+    ServiceLayer invoiceServiceLayer;
 
     @GetMapping("/invoices")
     @ResponseStatus(value = HttpStatus.OK)
@@ -38,7 +43,9 @@ public class InvoiceController {
 
     @PostMapping("/invoices")
     @ResponseStatus(HttpStatus.CREATED)
-    public Invoice createInvoice(@RequestBody Invoice invoice) {
-        return invoiceRepository.save(invoice);
+    public Invoice createInvoice(@RequestBody @Valid Invoice invoice) {
+        Invoice newInvoice = invoiceServiceLayer.saveInvoice(invoice);
+
+        return newInvoice;
     }
 }
