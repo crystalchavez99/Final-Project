795755336ccff5a195af1bc6c3ef9ce542e71b6f
Adrian Castro <acastrofarias13@gmail.com>
2023-08-26 13:57:16 -0700

    game controller and repo tests

--- src/test/java/com/company/gamestore/controller/GameControllerTest.java
+++ src/test/java/com/company/gamestore/controller/GameControllerTest.java
@@ -1,4 +1,114 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Game;
+import com.company.gamestore.repository.GameRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GameController.class)
 public class GameControllerTest {
+    @MockBean
+    private GameRepository gameRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Game game;
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setUp() throws Exception{
+        game = new Game();
+
+        BigDecimal decimal = new BigDecimal("24.99");
+        MathContext mc = new MathContext(4);
+
+        game.setTitle("Super Monkey Ball");
+        game.setEsrbRating("Everyone");
+        game.setDescription("Call your friends and warn your neighbors, it's time to have a ball! Go bananas with 90+ stages, multi-player madness, and 7 cool ways to play! Equal parts \"party\" and \"game\", Super Monkey Ball could be the most \"well-rounded\" game you've ever played!");
+        game.setPrice(decimal.round(mc));
+        game.setStudio("Amusement Vision");
+        game.setQuantity(5);
+
+        game = gameRepository.save(game);
+    }
+
+    @Test
+    void shouldCreateGame() throws Exception {
+        String input = mapper.writeValueAsString(game);
+
+        mockMvc.perform(post("/games")
+                        .content(input)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldGetAllGames() throws Exception {
+        mockMvc.perform(get("/games"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetGameById() throws Exception {
+        mockMvc.perform(get("/games/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateGame() throws Exception {
+        game.setTitle("Super Monkey Ball 2");
+        String inputJson = mapper.writeValueAsString(game);
+
+        mockMvc.perform(put("/games")
+                        .content(inputJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void deleteGameById() throws Exception {
+        mockMvc.perform(delete("/games/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void shouldGetGameByStudio() throws Exception {
+        mockMvc.perform(get("/games/studio/Amusement+Vision"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetGameByRating() throws Exception {
+        mockMvc.perform(get("/games/rating/Everyone"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetGameByTitle() throws Exception {
+        mockMvc.perform(get("/games/title/Super+Monkey+Ball"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/com/company/gamestore/repository/GameRepositoryTest.java
+++ src/test/java/com/company/gamestore/repository/GameRepositoryTest.java
@@ -1,4 +1,96 @@
 package com.company.gamestore.repository;
 
+import com.company.gamestore.model.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
 public class GameRepositoryTest {
+    @Autowired
+    private GameRepository gameRepository;
+
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        gameRepository.deleteAll();;
+
+        game = new Game();
+
+        BigDecimal decimal = new BigDecimal("24.99");
+        MathContext mc = new MathContext(4);
+
+        game.setTitle("Super Monkey Ball");
+        game.setEsrbRating("Everyone");
+        game.setDescription("Call your friends and warn your neighbors, it's time to have a ball! Go bananas with 90+ stages, multi-player madness, and 7 cool ways to play! Equal parts \"party\" and \"game\", Super Monkey Ball could be the most \"well-rounded\" game you've ever played!");
+        game.setPrice(decimal.round(mc));
+        game.setStudio("Amusement Vision");
+        game.setQuantity(5);
+
+        game = gameRepository.save(game);
+    }
+
+    @Test
+    void shouldCreateGame(){
+        Optional<Game> game2 = gameRepository.findById(game.getId());
+        assertEquals(game2.get(), game);
+    }
+
+    @Test
+    void shouldFindAllGames(){
+        List<Game> games = gameRepository.findAll();
+        assertEquals(1,games.size());
+    }
+
+    @Test
+    void shouldFindGameById() {
+        Optional<Game> game2 = gameRepository.findById(game.getId());
+        assertEquals(game2.get(), game);
+    }
+
+    @Test
+    void shouldUpdateGame(){
+        game.setTitle("Super Monkey Ball 2");
+
+        gameRepository.save(game);
+
+        Optional<Game> game2 = gameRepository.findById(game.getId());
+        assertEquals(game2.get(), game);
+    }
+
+    @Test
+    void shouldDeleteGameById(){
+        gameRepository.deleteById(game.getId());
+
+        Optional<Game> game2 = gameRepository.findById(game.getId());
+        assertFalse(game2.isPresent());
+    }
+
+    @Test
+    void shouldFindGameByStudio() {
+        List<Game> game2 = gameRepository.findByStudio(game.getStudio());
+        assertEquals(1, game2.size());
+    }
+
+    @Test
+    void shouldFindGameByRating() {
+        List<Game> game2 = gameRepository.findByRating(game.getEsrbRating());
+        assertEquals(1, game2.size());
+    }
+
+    @Test
+    void shouldFindGameByTitle() {
+        List<Game> game2 = gameRepository.findByTitle(game.getTitle());
+        assertEquals(1, game2.size());
+    }
 }
